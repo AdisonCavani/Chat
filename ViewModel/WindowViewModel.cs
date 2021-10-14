16766795526163d07340c4d64dd6eeb368e6b519
Adison Cavani <adriansrodon@tuta.io>
2021-10-14 20:46:09 +0200

    Initial commit

---
+++ ViewModel/WindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace Chat
+{
+    /// <summary>
+    /// The view model for the custom flat style
+    /// </summary>
+    public class WindowViewModel : BaseViewModel
+    {
+        #region Private Member
+        /// <summary>
+        /// The window this view model controls
+        /// </summary>
+        private Window mWindow;
+        #endregion
+
+        #region Public Properties
+        public string Text { get; set; } = "My string";
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="window"></param>
+        public WindowViewModel(Window window)
+        {
+
+        }
+        #endregion
+    }
+}
